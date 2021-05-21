2773e28b7c8355adf8c391f1117e2518b17d62bd
root <571948159@qq.com>
2021-05-21 10:57:09 +0800

    Flask练习/

--- Flask练习/guolvq.py
+++ Flask练习/guolvq.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/show')
 def show():
-    girls = ['11', '222', '33', '444', '5555']
+    girls = ['11', '222', '33', '444', '55']
     users = [
         {'username': '张三1', 'password': '111', 'addr': '北京', 'phone': '10086'},
         {'username': '张三2', 'password': '222', 'addr': '上海', 'phone': '10086'},
@@ -13,7 +13,10 @@ def show():
         {'username': '张三4', 'password': '444', 'addr': '深圳', 'phone': '10086'},
         {'username': '张三5', 'password': '555', 'addr': '曹县', 'phone': '10086'}
     ]
-    return render_template('One.html', girls=girls, users=users)
+    girls.append('zhangsan')
+    msg = '<h1>快乐</h1>'
+    n1 = 'hello world'
+    return render_template('guolvq.html', girls=girls, users=users, msg=msg, n1=n1)
 
 
 if __name__ == '__main__':
